6c0e06f2ff774f897484548839939353c3ef621b
Betanio Nascimento <betanio@betanio.com>
2012-04-15 19:32:37 -0300

    Entrega 04

--- src/core/admin.py
+++ src/core/admin.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from core.models import Speaker, Contact
+from core.models import Speaker, Talk, Course, Contact, Media
 
 
 class ContactInline(admin.TabularInline):
@@ -15,6 +15,17 @@ class SpeakerAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug' : ('name', )}
 
 
+class MediaInline(admin.TabularInline):
+    model = Media
+    extra = 1
+
+class TalkAdmin(admin.ModelAdmin):
+    inlines = [MediaInline, ]
+
+
 admin.site.register(Speaker, SpeakerAdmin)
+admin.site.register(Talk, TalkAdmin)
+admin.site.register(Course)
+admin.site.register(Media)
 
 
